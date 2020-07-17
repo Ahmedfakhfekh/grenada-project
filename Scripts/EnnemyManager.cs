a5176c550b06c9f486f5bb76ce31d822b600446a
Ahmedfakhfekh <ahmed.fakhfakh01@gmail.com>
2020-07-17 12:05:27 +0100

    beta game

---
+++ Scripts/EnnemyManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnnemyManager : MonoBehaviour
+{
+    public GameObject ennemy, panel;
+    public Transform ennemyTransform1, targ;
+    int health = 100;
+    public Text HealthBar;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "bullet")
+        {
+            Destroy(collision.gameObject);
+            health--;
+            HealthBar.text = health.ToString();
+        }
+        if(collision.gameObject.name == "player")
+        {
+            gameover();
+        }
+    }
+    void gameover()
+    {
+        Time.timeScale = 0;
+        panel.SetActive(true);
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1;
+    }
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    } 
+    public void quit()
+    {
+        Application.Quit();
+    }
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targ.transform.position, .005f);
+        if(health == 0)
+        {
+            gameover();
+        }
+    }
+}
+
---
+++ Scripts/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * 10;
+        rb.velocity = new Vector2(x, 0);
+        
+        if(Input.GetKeyDown("up"))
+        {
+            Instantiate(bullet, bulletTransform.position, bulletTransform.rotation);
+        }
+    }
+}
